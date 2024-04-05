a47c9880bb0c7b58af791942fe8bb9a5fcf18e2d
Kayme976 <7508@holbertonstudents.com>
2024-04-05 14:06:38 +0200

    Refactor models/__init__.py, models/city.py, models/amenity.py, models/review.py, models/state.py, models/user.py, tests/test_models/test_base_model.py, setup_mysql_dev.sql, setup_mysql_test.sql

--- models/state.py
+++ models/state.py
@@ -1,8 +1,25 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
 
 
-class State(BaseModel):
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models import FileStorage
+from models.city import City
+
+
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+
+    name = Column(String(128), nullable=False)
+    cities = relationship('City', cascade='all, delete', backref='state')
+
+    @property
+    def cities(self):
+        city_list = []
+        for city in FileStorage.all(City):
+            if city.state_id == self.id:
+                city_list.append(city)
+        return city_list
